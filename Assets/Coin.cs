78e44fa041a91c0061f9d2a18de1d4dbbb4a004f
mikever <mveronie@gmail.com>
2017-05-17 07:26:00 -0400

    Initial game

---
+++ Assets/Coin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    //Keeps track of total coin count in scene
+    public static int CoinCount = 0;
+
+	// Use this for initialization
+	void Start () {
+        //Object created, increment coin count
+        ++Coin.CoinCount;
+	}
+    
+    void OnTriggerEnter(Collider Col)
+    {
+        //If player collected coin, then destroy object
+        if (Col.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+
+    //Called when object destroyed
+    void OnDestroy()
+    {
+        //Decrement coin count
+        --Coin.CoinCount;
+
+        //Check remaining coins
+        if (Coin.CoinCount <= 0)
+        {
+            // Game is won. Collected all coins
+            // Destroy timer and launch fireworks
+            GameObject Timer = GameObject.Find("LevelTimer");
+            Destroy(Timer);
+
+            GameObject[] FireworkSystems =
+                GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach (GameObject GO in FireworkSystems)
+                GO.GetComponent<ParticleSystem>().Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+    // Maximum time to complete level (in seconds)
+    public float MaxTime = 60f;
+
+    //Countdown
+    [SerializeField]
+    private float CountDown = 0;
+
+	// Use this for initialization
+	void Start () {
+        CountDown = MaxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Reduce time
+        CountDown -= Time.deltaTime;
+
+        // Restart level if time runs out
+        if (CountDown <= 0)
+        {
+            // Reset coin count
+            Coin.CoinCount = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
