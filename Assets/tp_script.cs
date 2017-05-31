542b4b741cd693ab601b0a047264e9abdc2ab5d9
mikever <mveronie@gmail.com>
2017-05-31 07:02:39 -0400

    Added materials; added teleport portals

---
+++ Assets/tp_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tp_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //log message for my own tracking
+        Debug.Log("Portal Stone Created");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider Col)
+    {
+        //debug message letting me know I touched object
+        Debug.Log("You touched the stone");
+
+        Col.transform.position = new Vector3(-15.61f, 17.34f, 0);
+    }
+}
---
+++ Assets/tp_script2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tp_script2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //log message for my own tracking
+        Debug.Log("Portal Stone Created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider Col)
+    {
+        //debug message letting me know I touched object
+        Debug.Log("You touched the stone");
+
+        Col.transform.position = new Vector3(-77.68f, 8, -112.2f);
+    }
+}
